5a8877dd60e2c45bd776e3e23e8c79ee997cba14
18870765501 <2489755067@qq.com>
2019-12-13 19:43:51 +0800

    登录模块 -整合路由

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,32 +1,146 @@
 <template>
-    <div class="login-box">
-        <div class="from-box"></div>
-        <img class="login-src" src="../../assets/login_banner_ele.png" alt="">
+  <div class="login-box">
+    <!-- 表单容器 -->
+    <div class="from-box">
+      <div class="title-box">
+        <img src="../../assets/logon.png" alt />
+        <span class="titlt">黑马面面</span>
+        <p></p>
+        <span class="title-login">用户登录</span>
+      </div>
+      <!-- 搜索框 -->
+      <el-form class="form-box" ref="form" :model="form">
+        <el-form-item>
+          <el-input placeholder="请输入手机号" prefix-icon="el-icon-user" v-model="form.name1"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入密码" prefix-icon="el-icon-lock" v-model="form.name2" show-password></el-input>
+        </el-form-item>
+        <el-form-item>
+          <!-- 行 -->
+          <el-row>
+            <!-- 列 -->
+            <el-col :span="18">
+              <el-input placeholder="请输入验证码" prefix-icon="el-icon-key" v-model="form.name3"></el-input>
+            </el-col>
+            <el-col :span="6">
+              <img class="verification" src="../../assets/verification.png" alt />
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-checkbox-group class="checkbox" v-model="form.type">
+          <el-checkbox class="cleckbox-txt" name="type">
+            我已阅读并同意
+            <el-link class="cleckbox-title" type="primary">用户协议</el-link>和
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-checkbox-group>
+        <!-- 按钮 -->
+        <el-form-item>
+          <el-button class="btn-txt" type="primary">登录</el-button>
+          <el-button type="primary">注册</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+    <img class="login-src" src="../../assets/login_banner_ele.png" alt />
+  </div>
+  
 </template>
 
 <script>
 export default {
-    name:"login"
-}
+  name: "login",
+  data() {
+    return {
+      form: {
+        name1: "",//手机
+        name2: "",//密码
+        name3: "",//验证码
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      window.console.log("submit!");
+    }
+  }
+};
 </script>
-
 <style lang="less">
-.login-box{
-    // 开启弹性布局
-    display: flex;
-    /* //上下居中 */
-    align-items: center;
-    //左右居中
-    justify-content: space-around;
-    background:linear-gradient(225deg,rgba(20,147,250,1),rgba(1,198,250,1));
-    height: 100%;
-    .from-box{
+.login-box {
+  //开启弹性布局
+  display: flex;
+  //上下居中
+  align-items: center;
+  //左右居中
+  justify-content: space-around;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+  height: 100%;
+  .from-box {
     width: 478px;
     height: 550px;
-    background-color: #F5F5F5;
+    background-color: #f5f5f5;
+    padding: 44px;
+    .title-box {
+      display: flex;
+      align-items: center;
+      margin-left: 4px;
+      margin-bottom: 29px;
+      img {
+        width: 22px;
+        height: 17px;
+      }
+      .titlt {
+        margin-left: 20px;
+        font-size: 24px;
+        margin-right: 12px;
+      }
+      p {
+        width: 1px;
+        height: 28px;
+        background-color: #c7c7c7;
+        margin-right: 12px;
+      }
+      .title-login {
+        font-size: 22px;
+      }
     }
+    //表单元素
+    .form-box {
+      // 验证码
+      .verification {
+        height: 40px;
+        width: 100%;
+      }
+      .el-checkbox{
+        display: flex;
+        align-items: center;
+        .el-checkbox__label {
+          display: flex;
+          align-items: center;
+        }
+      }
+     
+    }
+    .el-button{
+        width: 100%;
+        margin: 0;
+      }
+    .btn-txt{
+      margin-top: 28px;
+      margin-bottom: 26px;
+    }
+  }
 }
-
-
 </style>
---
+++ src/views/login/register.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="register">
+    <div class="register-title">
+      <span>用户注册</span>
+    </div>
+    <div class="icon">
+      <p>头像</p>
+      <div class="icon-box"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="less">
+.register {
+  width: 600px;
+  height: 100%;
+  margin: 0 auto;
+  // display: block;
+  // text-align: center;
+  .register-title {
+    width: 100%;
+    height: 60px;
+    background-color: deepskyblue;
+    span {
+      font-size: 18px;
+      color: #fefefe;
+      display: block;
+      text-align: center;
+      line-height: 60px;
+    }
+  }
+  .icon {
+    p {
+      font-size: 15px;
+      margin-top: 39px;
+      margin-left: 34px;
+    }
+    .icon-box {
+      width: 179px;
+      height: 179px;
+      border: 1px dashed #cecece;
+      margin: 26px 186px 48px 236px;
+    }
+  }
+}
+</style>
