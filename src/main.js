63058fbf54c3dab01750dd82c404b8ba795ee11d
18870765501 <2489755067@qq.com>
2019-12-13 19:47:56 +0800

    登录模块 -文件创建

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 // 导入 Vue
 import Vue from 'vue'
+// 导入 根组件
+import App from '@/App.vue'
+// 导入 路由
+import router from '@/router'
 // 导入 Element-ui
 import ElementUI from 'element-ui';
 // 导入 Element-ui的样式
 import 'element-ui/lib/theme-chalk/index.css';
 //导入初始化代码
 import './style/base.css';
-
-// 导入 根组件
-import App from '@/App.vue'
-// 导入 路由
-import router from '@/router'
 //是否显示生产提示
 Vue.config.productionTip = false
 // 注册 Element-ui
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 //1.导入Vue
 import Vue from 'vue';
+//导包
 import VueRouter from "vue-router"
 //导入登录页面
 import login from "../views/login/login.vue"
+import register from "../views/login/register.vue"
 //注册
 Vue.use(VueRouter)
 //规则
@@ -10,6 +12,10 @@ const routes = [
     {
         path:"/login",
         component:login
+    },
+    {
+        path:"/register",  //路径
+        component:register //文件名
     }
 ]
 //创建路由对象
